095bd8316e2a28880989bf1aabb565455b916be9
TalentUnlimited <122269523+TalentUnlimited@users.noreply.github.com>
2023-10-17 15:26:34 +0530

    alphabetaRoot() added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import chess
+import numpy
 
 board = chess.Board()
 
@@ -41,17 +42,22 @@ def negamax(board, depth):
 
 	return best_evaluation
 
-def minimax(board, depth, alpha, beta, maximizingPlayer):
+def alphabeta(board, depth, alpha, beta, maximizingPlayer):
 	if depth == 0:
 		return evaluate(board)
 	
+	if len(list(board.legal_moves)) == 0:
+		if board.is_check:
+			return -999
+		return 0
+
 	if maximizingPlayer:
 		maxEval = -999
 		moves = list(board.legal_moves)
 
 		for move in moves:
 			board.push_san(str(move))
-			evaluation = minimax(board, depth-1, alpha, beta, False)
+			evaluation = alphabeta(board, depth-1, alpha, beta, False)
 			maxEval = max(maxEval, evaluation)
 			alpha = max(alpha, evaluation)
 			board.pop()
@@ -65,7 +71,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 
 		for move in moves:
 			board.push_san(str(move))
-			evaluation = minimax(board, depth-1, alpha, beta, True)
+			evaluation = alphabeta(board, depth-1, alpha, beta, True)
 			minEval = min(minEval, evaluation)
 			beta = min(beta, evaluation)
 			board.pop()
@@ -74,6 +80,29 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 		
 		return minEval
 
+def alphabetaRoot(board, depth):
+	moves = list(board.legal_moves)
 
-print(minimax(chess.Board(fen='3r1rk1/ppp2pp1/3pb2p/6q1/3RP3/P4P2/1PPQB1PP/2K4R w - - 5 16'), 4, -999, 999, True))
-print(negamax(chess.Board(fen='3r1rk1/ppp2pp1/3pb2p/6q1/3RP3/P4P2/1PPQB1PP/2K4R w - - 5 16'), 4))
+	best_evaluation = -999
+	best_move = None
+
+	for move in moves:
+		board.push_san(str(move))
+		#print(move, '\n', board)
+		evaluation = alphabeta(board, depth-1, -999, 999, board.turn)
+
+		board.pop()
+
+		if evaluation > best_evaluation:
+			best_evaluation = evaluation
+			best_move = move
+	
+	return best_move
+
+def ordermoves(moves):
+	pass
+
+board = chess.Board(fen='rnbqkb1r/pppp1pp1/4pn1p/6B1/3PP3/8/PPP2PPP/RN1QKBNR w KQkq - 0 4')
+
+
+print(alphabetaRoot(board, 5))
