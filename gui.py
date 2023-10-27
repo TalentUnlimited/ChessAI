50f03c32c87004d0355a751a97db30f56d093dc5
TalentUnlimited <122269523+TalentUnlimited@users.noreply.github.com>
2023-10-27 22:42:08 +0530

    Highlighting Moves & Minor Change To Move Functionality

--- gui.py
+++ gui.py
@@ -16,6 +16,7 @@ LEFT = 1
 RIGHT = 3
 
 move = ""
+highlighted = []
 
 while running:
 
@@ -30,17 +31,41 @@ while running:
 
 				if len(move) == 0:
 					move += square
+					updateHighlighted(highlighted, square)
 				elif len(move) == 2:
 					pseudo_move = move + square
 					if (square != move) and chess.Move.from_uci(pseudo_move) in board.legal_moves:
 						move += square
 						makeMove(move)
+						move = ""
+						highlighted = []
+						renderBoard()
 						if len(list(board.legal_moves)) != 0:
-							makeComputerMove(4)
-						move = ""
+							makeComputerMove(5)
 					else:
-						print(f"invalid move - {pseudo_move} is not possible")
-						move = ""
+						if move != square:
+							movingsquare = board.piece_at(chess.Move.from_uci(pseudo_move).from_square)
+							goingsquare = board.piece_at(chess.Move.from_uci(pseudo_move).to_square)
+							if (movingsquare != None and goingsquare != None) and movingsquare.color == goingsquare.color:
+								move = square
+								highlighted = []
+								updateHighlighted(highlighted, square)
+							else:
+								move = ""
+								highlighted = []
+								print(f"invalid move - {pseudo_move} is not possible")	
+						else:
+							move = ""
+							highlighted = []
+							print(f"invalid move - {pseudo_move} is not possible")
+
+	def updateHighlighted(highlighted, square):
+		if square == []:
+			highlighted = []
+		else:
+			for legal_move in board.legal_moves:
+				if legal_move.from_square == chess.parse_square(square):
+					highlighted.append(legal_move.to_square)
 
 	def makeMove(move):
 		board.push_san(move)
@@ -53,8 +78,8 @@ while running:
 
 	def renderBoard():
 		win.fill((255,255,255))
-		for y in range(9):
-			for x in range(9):
+		for y in range(8):
+			for x in range(8):
 				if (x+y) % 2 == 0:
 					pygame.draw.rect(win,(234, 233, 210),pygame.Rect(x*(WIDTH/8),y*(HEIGHT/8),WIDTH/8,HEIGHT/8))
 				else:
@@ -80,6 +105,9 @@ while running:
 			
 		for y in range(8):
 			for x in range(8):
+				
+				if chess.parse_square(f"{chr(97+x)}{8-y}") in highlighted:
+					pygame.draw.rect(win,(153, 153, 255),pygame.Rect(x*(WIDTH/8),y*(HEIGHT/8),WIDTH/8,HEIGHT/8))
 				if board_matrix[y][x] != ' ':
 					i = pygame.transform.scale(pygame.image.load(f"D:/ChessAI/Chess Pieces/{board_matrix[y][x]}.png"),(int(WIDTH/8), int(HEIGHT/8)))
 					win.blit(i, (x*(WIDTH/8),y*(HEIGHT/8)))
